1b40388cfb13a2ef7f1a1afa60d76bc27f105d06
frontendmonster <frontendmonster@gmail.com>
2019-06-15 03:35:32 +0430

    fix: fix module resolve issue
    Add unminified umd version

--- rollup.config.js
+++ rollup.config.js
@@ -4,28 +4,35 @@ import nodeResolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import { uglify } from 'rollup-plugin-uglify'
 import pkg from './package.json'
+import { getIfUtils, removeEmpty } from 'webpack-config-utils'
 
+const env = process.env.NODE_ENV || 'development';
+const { ifProduction } = getIfUtils(env)
 const lib = resolve(__dirname, 'lib')
 const peerDependencies = pkg.peerDependencies || [];
 const external = Object.keys(peerDependencies) || []
-const renameMin = (filename) => filename.replace(/\.js$/, '.min.js');
+
+const getOutputFileName = (filename) =>
+  ifProduction()
+    ? filename.replace(/\.js$/, '.min.js')
+    : filename;
 
 const config = {
   input: resolve(lib, 'esm5', 'index.js'),
   output: {
-    file: renameMin(resolve(lib, 'bundles', 'index.umd.js')),
+    file: getOutputFileName(resolve(lib, 'bundles', 'index.umd.js')),
     format: 'umd',
     name: 'falorem',
     sourcemap: true,
   },
   inlineDynamicImports: true,
   external,
-  plugins: [
+  plugins: removeEmpty([
     commonjs(),
     nodeResolve(),
     sourceMaps(),
-    uglify()
-  ]
+    ifProduction(uglify())
+  ])
 }
 
 export default config;
